c6b9713d4b5eb0aa577666210c099c9190814cc7
FairyScript <h@queb.fun>
2021-01-19 02:05:29 +0800

    add react-refresh

--- config/webpack.renderer.config.js
+++ config/webpack.renderer.config.js
@@ -1,13 +1,31 @@
-module.exports = {
-  //fix reload
-  devServer: {
-    historyApiFallback: {
-      disableDotRule: true
-    }
-  },
-  //fix react
-  externals: {
-    react: 'react',
-    reactDOM: 'react-dom',
-  }
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = config => {
+  const isDevelopment = process.env.NODE_ENV !== 'production';
+
+  config.devServer.historyApiFallback = { disableDotRule: true };
+  config.externals.push(...['react', 'react-dom']);
+  
+  //react-refresh must be loaded before other loaders
+  config.module.rules.unshift({
+    test: /\.[jt]sx?$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: require.resolve('babel-loader'),
+        options: {
+          plugins: [
+            isDevelopment && [require.resolve('react-refresh/babel'), {
+              skipEnvCheck: true,
+            }],
+          ].filter(Boolean)
+        },
+      },
+    ],
+  });
+  config.plugins.push(...[
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+  ].filter(Boolean));
+
+  return config;
 };
