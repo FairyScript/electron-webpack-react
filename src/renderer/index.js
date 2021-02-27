b9e6a6efd7c579b37ad7fba59dbb0be9cc0f7a98
FairyScript <h@queb.fun>
2021-02-27 18:19:54 +0800

    fix tsconfig warning

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 //fast refresh
 if (module.hot) {
   module.hot.accept();
-}
-
-function App() {
-  return <h1>Hello World!</h1>
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
